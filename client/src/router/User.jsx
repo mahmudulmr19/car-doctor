a5dd1377580dc9507e452d91a56088c2b167f4c3
Mahmudul Hasan <mahmudulmr19@gmail.com>
2023-05-16 03:50:37 +0600

    weg

--- client/src/router/User.jsx
+++ client/src/router/User.jsx
@@ -5,7 +5,7 @@ import { Navigate } from "react-router-dom";
 const User = ({ children }) => {
   const { currentUser, loading } = useContext(AuthContext);
   if (loading) {
-    return <h1>Loadin</h1>;
+    return <h1>Loading</h1>;
   }
   if (currentUser) {
     return children;
